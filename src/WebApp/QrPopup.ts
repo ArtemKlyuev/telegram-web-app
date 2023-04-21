8cc61e89b5c1cb131cb313bac7c987d47171b842
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-04-21 20:37:35 +0400

    feat: add `QrPopup`

---
+++ src/WebApp/QrPopup.ts
@@ -0,0 +1,57 @@
+import { ScanQrCallback, ScanQrPopupParams } from './types';
+
+interface Options {
+  params: ScanQrPopupParams;
+  callback?: ScanQrCallback | null | undefined;
+}
+
+const INITIAL_DATA: Options = { params: {}, callback: null };
+
+export class QrPopup {
+  #isOpened = false;
+
+  #data: Options = INITIAL_DATA;
+
+  static get MAX_TEXT_LENGTH(): number {
+    return 64;
+  }
+
+  open({ params: { text = '' }, callback }: Options): void {
+    if (this.#isOpened) {
+      console.error('[Telegram.WebApp] Popup is already opened');
+      throw new Error('WebAppScanQrPopupOpened');
+    }
+
+    const trimmedText = text.trim();
+
+    if (trimmedText.length > QrPopup.MAX_TEXT_LENGTH) {
+      console.error('[Telegram.WebApp] Scan QR popup text is too long', text);
+      throw new Error('WebAppScanQrPopupParamInvalid');
+    }
+
+    if (trimmedText) {
+      this.#data.params.text = trimmedText;
+    }
+
+    this.#data.callback = callback;
+
+    this.#isOpened = true;
+  }
+
+  close(): void {
+    this.#isOpened = false;
+    this.#data = INITIAL_DATA;
+  }
+
+  get isOpened(): boolean {
+    return this.#isOpened;
+  }
+
+  get params(): ScanQrPopupParams {
+    return this.#data.params;
+  }
+
+  get callback(): ScanQrCallback | null {
+    return this.#data.callback ?? null;
+  }
+}
