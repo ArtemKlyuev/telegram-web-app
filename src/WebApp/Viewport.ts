c02ff5e3389395bf987d7981cab4dde43b934440
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-04-24 21:27:33 +0400

    refactor: add `EventEmitter` to `Viewport`

--- src/WebApp/Viewport.ts
+++ src/WebApp/Viewport.ts
@@ -1,11 +1,9 @@
+import { ValueOf } from '../types';
+import { Disposer, EventEmitter } from '../utils';
+
 interface Options {
+  eventEmitter: EventEmitter<ViewportEvent>;
   mainButtonHeight: number;
-  // FIXME: decouple this and use event emitter
-  callbacks: {
-    webViewCallback: (isStateStable: boolean) => any;
-    setCssHeight: (height: string) => any;
-    setCssStableHeight: (height: string) => any;
-  };
 }
 
 export interface ViewportData {
@@ -14,15 +12,27 @@ export interface ViewportData {
   is_state_stable: boolean;
 }
 
+type ViewportEvents = typeof Viewport.EVENTS;
+type ViewportEvent = ValueOf<ViewportEvents>;
+type ViewportChangeListener = (isStateStable: boolean) => any;
+type HeightCalculatedListener = (params: { height: number; stableHeight: number }) => any;
+
 export class Viewport {
+  #eventEmitter: EventEmitter<ViewportEvent>;
   #viewportHeight: number | false = false;
   #viewportStableHeight: number | false = false;
   #isExpanded: boolean = true;
   #mainButtonHeight: number;
-  #callbacks: Options['callbacks'];
 
-  constructor({ callbacks, mainButtonHeight }: Options) {
-    this.#callbacks = callbacks;
+  static get EVENTS() {
+    return {
+      VIEWPORT_CHANGED: 'viewport_changed',
+      HEIGHT_CALCULATED: 'height_calculated',
+    } as const;
+  }
+
+  constructor({ eventEmitter, mainButtonHeight }: Options) {
+    this.#eventEmitter = eventEmitter;
     this.#mainButtonHeight = mainButtonHeight;
   }
 
@@ -40,6 +50,12 @@ export class Viewport {
     return height - this.#mainButtonHeight;
   }
 
+  on(event: ViewportEvents['HEIGHT_CALCULATED'], listener: HeightCalculatedListener): Disposer;
+  on(event: ViewportEvents['VIEWPORT_CHANGED'], listener: ViewportChangeListener): Disposer;
+  on(event: ViewportEvent, listener: HeightCalculatedListener | ViewportChangeListener): Disposer {
+    return this.#eventEmitter.subscribe(event, listener);
+  }
+
   setHeight = (data?: ViewportData | undefined): void => {
     if (typeof data !== 'undefined') {
       this.#isExpanded = Boolean(data.is_expanded);
@@ -49,14 +65,13 @@ export class Viewport {
         this.#viewportStableHeight = data.height;
       }
 
-      this.#callbacks.webViewCallback(Boolean(data.is_state_stable));
+      this.#eventEmitter.emit(Viewport.EVENTS.VIEWPORT_CHANGED, Boolean(data.is_state_stable));
     }
 
     const height = this.#calculateHeight(this.#viewportHeight);
-    const stable_height = this.#calculateHeight(this.#viewportStableHeight);
+    const stableHeight = this.#calculateHeight(this.#viewportStableHeight);
 
-    this.#callbacks.setCssHeight(height);
-    this.#callbacks.setCssStableHeight(stable_height);
+    this.#eventEmitter.emit(Viewport.EVENTS.HEIGHT_CALCULATED, { height, stableHeight });
   };
 
   get isExpanded(): boolean {
