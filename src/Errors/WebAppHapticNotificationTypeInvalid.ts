64321f33c7a585f0aff3ec33f7aa923b73d42ee5
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-04-27 00:44:40 +0400

    refactor: `WebAppHapticNotificationTypeInvalidError`

--- src/Errors/WebAppHapticNotificationTypeInvalid.ts
+++ src/Errors/WebAppHapticNotificationTypeInvalid.ts
@@ -1,7 +1,5 @@
-import { HapticFeedbackNotification } from '../types';
-
 export class WebAppHapticNotificationTypeInvalidError extends Error {
-  constructor(notification: HapticFeedbackNotification) {
+  constructor(notification: any) {
     super(`[Telegram.WebApp] Haptic notification type is invalid ${notification}`);
   }
 }
