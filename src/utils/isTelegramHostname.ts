e5b91e700cc586834888f721e57ae9fc00d3ded0
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-04-27 17:17:25 +0400

    feat: add additional telegram domains

--- src/utils/isTelegramHostname.ts
+++ src/utils/isTelegramHostname.ts
@@ -1,5 +1,18 @@
 const VALID_TELEGRAM_HOSTNAMES = ['t.me', 'telegram.me', 'telegram.dog'];
 
+const isHostnameWithUsername = (hostname: string): boolean => {
+  // for example username.t.me
+  return hostname.split('.').length > 2;
+};
+
 export const isTelegramHostname = (hostname: string): boolean => {
-  return VALID_TELEGRAM_HOSTNAMES.includes(hostname.toLowerCase());
+  let lowerCaseHostname = hostname.toLowerCase();
+  // for example username.t.me
+  if (isHostnameWithUsername(lowerCaseHostname)) {
+    const [subdomain, domain] = hostname.split('.').reverse();
+
+    lowerCaseHostname = `${subdomain}.${domain}`;
+  }
+
+  return VALID_TELEGRAM_HOSTNAMES.includes(lowerCaseHostname);
 };
