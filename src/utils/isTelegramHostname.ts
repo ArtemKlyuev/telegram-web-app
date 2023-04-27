8f4487ddca270b8d19992b13f789be353a8929f0
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-04-27 16:56:33 +0400

    feat: add `VALID_TELEGRAM_HOSTNAMES`

--- src/utils/isTelegramHostname.ts
+++ src/utils/isTelegramHostname.ts
@@ -1,1 +1,5 @@
-export const isTelegramHostname = (hostname: string): boolean => hostname.toLowerCase() === 't.me';
+const VALID_TELEGRAM_HOSTNAMES = ['t.me', 'telegram.me', 'telegram.dog'];
+
+export const isTelegramHostname = (hostname: string): boolean => {
+  return VALID_TELEGRAM_HOSTNAMES.includes(hostname.toLowerCase());
+};
