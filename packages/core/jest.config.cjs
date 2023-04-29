25e6d334911f3667175a9ebae1d2899990e5cd43
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-04-29 21:23:20 +0400

    chore: update `jest` config

--- packages/core/jest.config.cjs
+++ packages/core/jest.config.cjs
@@ -1,12 +1,27 @@
 /** @type {import('jest').Config} */
 const config = {
-  clearMocks: true,
-  collectCoverage: false,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
+  moduleNameMapper: {
+    '^@typings/common$': '<rootDir>/src/typings/common.ts',
+    '^@typings/utils$': '<rootDir>/src/typings/utils.ts',
+    '^@typings/WebApp$': '<rootDir>/src/typings/WebApp.ts',
+    '^@typings/WebView$': '<rootDir>/src/typings/WebView.ts',
+    '^@Errors$': '<rootDir>/src/Errors/index.ts',
+    '^@decorators$': '<rootDir>/src/decorators/index.ts',
+    '^@utils$': '<rootDir>/src/utils/index.ts',
+  },
+  testEnvironment: 'jsdom',
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['./src/**'],
+  coverageThreshold: {
+    global: {
+      lines: 90,
+    },
   },
 };
 
