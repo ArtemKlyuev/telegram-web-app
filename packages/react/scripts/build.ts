514fb4ba62cc0d12fcc0d8dd66fa55b5f1b483b4
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-05-02 14:06:34 +0400

    build: add `build` script

---
+++ packages/react/scripts/build.ts
@@ -0,0 +1,54 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { build, BuildOptions } from 'esbuild';
+import { dTSPathAliasPlugin } from 'esbuild-plugin-d-ts-path-alias';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const DIST_DIR = 'dist';
+const DIST_DIR_PATH = path.resolve(process.cwd(), DIST_DIR);
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const baseOptions: BuildOptions = {
+  platform: 'browser',
+  target: 'es2020',
+  entryPoints: ['./src/index.ts'],
+  plugins: [dTSPathAliasPlugin({ debug: true })],
+  bundle: true,
+  treeShaking: true,
+  sourcemap: isDev,
+  minify: false,
+};
+
+await fs.rm(DIST_DIR, { force: true, recursive: true });
+
+const esmBuild = build({
+  ...baseOptions,
+  splitting: true,
+  format: 'esm',
+  outdir: `${DIST_DIR}/esm`,
+});
+
+const cjsDevBuild = build({
+  ...baseOptions,
+  format: 'cjs',
+  outdir: `${DIST_DIR}/cjs/dev`,
+});
+
+const cjsProdBuild = build({
+  ...baseOptions,
+  format: 'cjs',
+  minify: true,
+  outdir: `${DIST_DIR}/cjs/prod`,
+});
+
+await Promise.all([esmBuild, cjsDevBuild, cjsProdBuild]);
+
+await fs.copyFile(
+  path.resolve(__dirname, './index-cjs.build.js'),
+  path.resolve(DIST_DIR_PATH, 'cjs/index.js'),
+);
