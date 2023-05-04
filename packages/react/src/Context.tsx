a7b64f33fe6b3281de710a34af42cc0ce0e693c0
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-05-04 10:28:10 +0400

    feat: add `autoReady` prop

--- packages/react/src/Context.tsx
+++ packages/react/src/Context.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, createContext } from 'react';
+import { FC, ReactNode, createContext, useEffect } from 'react';
 import { Telegram } from '@telegram-web-app/core/types';
 
 import { useCreateTelegramWebApp } from './useCreateTelegramWebApp';
@@ -6,12 +6,23 @@ import { useCreateTelegramWebApp } from './useCreateTelegramWebApp';
 interface Props {
   children: ReactNode;
   exposeInMainWorld?: boolean | undefined;
+  autoReady?: boolean | undefined;
 }
 
 export const TelegramWebAppContext = createContext<Telegram | null>(null);
 
-export const TelegramWebAppProvider: FC<Props> = ({ children, exposeInMainWorld = false }) => {
+export const TelegramWebAppProvider: FC<Props> = ({
+  children,
+  exposeInMainWorld = false,
+  autoReady = true,
+}) => {
   const telegram = useCreateTelegramWebApp({ exposeInMainWorld });
+
+  useEffect(() => {
+    if (autoReady) {
+      telegram.WebApp.ready();
+    }
+  }, []);
 
   return (
     <TelegramWebAppContext.Provider value={telegram}>{children}</TelegramWebAppContext.Provider>
