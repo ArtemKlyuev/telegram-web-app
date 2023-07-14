0525de4e7efae1de830e79b3743fb884f59fbe4c
97albertus <131011547+97albertus@users.noreply.github.com>
2023-07-14 21:09:33 +0500

    Fix .at() on older browsers
    Added a fallback function for the .at() method, this solves the issue with '.at() is not a function' error on older browsers (i.e. telegram desktop's built-in browser)

--- packages/core/src/WebApp/InitData/InitData.ts
+++ packages/core/src/WebApp/InitData/InitData.ts
@@ -3,6 +3,26 @@ import { WebAppInitData } from '@typings/WebApp';
 
 import { urlParseQueryString } from '@utils';
 
+function at(n) {
+  // ToInteger() abstract op
+  n = Math.trunc(n) || 0;
+  // Allow negative indexing from the end
+  if (n < 0) n += this.length;
+  // OOB access is guaranteed to return undefined
+  if (n < 0 || n >= this.length) return undefined;
+  // Otherwise, this is just normal property access
+  return this[n];
+}
+
+const TypedArray = Reflect.getPrototypeOf(Int8Array);
+for (const C of [Array, String, TypedArray]) {
+  Object.defineProperty(C.prototype, "at",
+                        { value: at,
+                          writable: true,
+                          enumerable: false,
+                          configurable: true });
+}
+
 type WebAppInitDataValues = ValueOf<WebAppInitData>;
 
 const isWrappedIn = (value: string, open: string, close: string): boolean => {
